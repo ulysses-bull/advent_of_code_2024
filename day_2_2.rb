d491a53bc6d1726014582f3c5c5fcc2aa552c27c
ulysses <ulyssesh.20@gmail.com>
2024-12-23 17:13:52 -0300

    comecei

--- day_2_2.rb
+++ day_2_2.rb
@@ -26,11 +26,19 @@ def valid_difference?(report)
   true
 end
 
+def remove_bad_level(report)
+end
+
 file = File.read("input_day_2.txt").split("\n")
 reports = file.map { _1.split(" ").map(&:to_i) }
 
 reports_analyze = reports.map do |report|
-  (increasing?(report) || decreasing?(report)) && valid_difference?(report)
+  if (increasing?(report) || decreasing?(report)) && valid_difference?(report)
+    true
+  else
+    new_report = remove_bad_level(report)
+    (increasing?(new_report) || decreasing?(new_report)) && valid_difference?(new_report)
+  end
 end
 
 puts reports_analyze.count(true)
