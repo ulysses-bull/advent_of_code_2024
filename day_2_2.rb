cb2f298d60850e1987c89e1aacbe7262bcb2dcbd
ulysses <ulyssesh.20@gmail.com>
2024-12-30 22:50:53 -0300

    dia 2 parte 2

--- day_2_2.rb
+++ day_2_2.rb
@@ -26,9 +26,7 @@ def valid_difference?(report)
   true
 end
 
-def remove_bad_level(report)
-end
-
+# file = File.read("sample_2.txt").split("\n")
 file = File.read("input_day_2.txt").split("\n")
 reports = file.map { _1.split(" ").map(&:to_i) }
 
@@ -36,8 +34,10 @@ reports_analyze = reports.map do |report|
   if (increasing?(report) || decreasing?(report)) && valid_difference?(report)
     true
   else
-    new_report = remove_bad_level(report)
-    (increasing?(new_report) || decreasing?(new_report)) && valid_difference?(new_report)
+    report.map.with_index do |level, index|
+      new_report = report.reject.with_index{|v, i| i == index }
+      (increasing?(new_report) || decreasing?(new_report)) && valid_difference?(new_report)
+    end.any?
   end
 end
 
