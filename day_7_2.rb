66314ecda6f85daa37b26665f98af2b73124dd13
ulysses <ulyssesh.20@gmail.com>
2025-01-15 11:40:12 -0300

    dia 7 parte 2

---
+++ day_7_2.rb
@@ -0,0 +1,42 @@
+def evaluate(string)
+  if string[1] == "||"
+    string[0] + string[2]
+  else
+    eval(string.join)
+  end
+end
+
+# file = File.read("sample_7.txt").split("\n")
+file = File.read("input_day_7.txt").split("\n")
+
+valid_test_values = file.map do |equation|
+  test_value = equation.split(":").first.to_i
+  numbers = equation.split(":").last.strip
+
+  p_operators = ["+", "*", "||"].repeated_permutation(numbers.count(" ")).to_a
+
+  operation_results = false
+  p_operators.map do |operators|
+    dup_numbers = numbers.dup
+    numbers.count(" ").times do |i|
+      blank_space = dup_numbers.index(" ")
+      dup_numbers[blank_space] = operators.shift
+    end
+    # binding.irb if dup_numbers == "6*8||6*15"
+    dup_numbers.gsub!("+", " + ")
+    dup_numbers.gsub!("*", " * ")
+    dup_numbers.gsub!("||", " || ")
+    conta = dup_numbers.split
+    while conta.length > 2
+      res = evaluate(conta[0..2])
+      conta[0..2] = res.to_s
+    end
+    if conta[0].to_i == test_value
+      operation_results = true
+      break
+    end
+  end
+  test_value if operation_results
+end.compact
+
+puts valid_test_values.sum
