7ccce0147f981972cf77d042d5b0a6cd7125bffd
ulysses <ulyssesh.20@gmail.com>
2025-01-15 09:50:36 -0300

    dia 7 parte 1

--- day_7.rb
+++ day_7.rb
@@ -1,13 +1,12 @@
-file = File.read("sample_7.txt").split("\n")
+# file = File.read("sample_7.txt").split("\n")
+file = File.read("input_day_7.txt").split("\n")
 
 valid_test_values = file.map do |equation|
   test_value = equation.split(":").first.to_i
   numbers = equation.split(":").last.strip
 
-  p_operators = ["+", "*"].permutation(numbers.count(" ")).to_a
-
+  p_operators = ["+", "*"].repeated_permutation(numbers.count(" ")).to_a
   operation_results = p_operators.map do |operators|
-    binding.irb
     dup_numbers = numbers.dup
     numbers.count(" ").times do |i|
       blank_space = dup_numbers.index(" ")
@@ -20,12 +19,9 @@ valid_test_values = file.map do |equation|
       res = eval(conta[0..2].join)
       conta[0..2] = res
     end
-    # result = eval(dup_numbers)
-    # result == test_value
     conta[0] == test_value
   end
-  # binding.irb
   test_value if operation_results.any?
-end
+end.compact
 
-binding.irb
+puts valid_test_values.sum
